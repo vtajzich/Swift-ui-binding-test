c11751f90e8663e3d64fbba3c631c1e8d6f9a1d3
Vitek Tajzich <v.tajzich@gmail.com>
2022-06-13 19:30:48 +0200

    Initial Commit

---
+++ SwiftUiBindingTest/SwiftUiBindingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiBindingTestApp.swift
+//  SwiftUiBindingTest
+//
+//  Created by Vitek Tajzich on 13.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiBindingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
